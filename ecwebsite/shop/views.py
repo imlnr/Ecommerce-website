0834f4095172ecc24db7a8d7cf908626d966ca3a
imlnr <bablureddy553@gmail.com>
2023-07-11 20:00:08 +0530

    final commit

--- ecwebsite/shop/views.py
+++ ecwebsite/shop/views.py
@@ -5,13 +5,19 @@ from math import ceil
 # Create your views here.
 
 def index(request):
-    products= Product.objects.all()
-    n= len(products)
-    nSlides= n//4 + ceil((n/4)-(n//4))
-    allProds=[[products, range(1, len(products)), nSlides],[products, range(1, len(products)), nSlides]]
-    params={'allProds':allProds }
+    # products= Product.objects.all()
+    allprods = []
+    catprods = Product.objects.values('category','id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category = cat)
+        n= len(prod)
+        nSlides= n//4 + ceil((n/4)-(n//4))
+        allprods.append([prod,range(1,nSlides),nSlides])
+    # allProds=[[products, range(1, len(products)), nSlides],[products, range(1, len(products)), nSlides]]
+    params={'allProds':allprods}
     return render(request,"shop/index.html", params)
-
+ 
 def about(request):
     return render(request, 'shop/about.html')
 def contact(request):
